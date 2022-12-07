3fa1fc7d446e490b4bd804db92e6e8229610c7e0
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-12-07 15:24:28 -0300

    new

---
+++ organo/src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import "./banner.css"
+
+export default function Banner(){
+
+    return(
+        <header className="banner">
+            <h1>Pessoas e times</h1>
+            <h4>organizados em um só lugar!</h4>
+            <img className="banner_img" src="img/banner.png" alt="banner" />
+        </header>
+    )
+}
