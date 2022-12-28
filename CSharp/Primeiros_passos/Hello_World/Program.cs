acd599f4419e7d55d849a1a39d7f6658b01fef33
Juliu Cesar <juliu2012@gmail.com>
2022-12-28 16:16:21 -0300

    new

--- CSharp/Primeiros_passos/Hello_World/Program.cs
+++ CSharp/Primeiros_passos/Hello_World/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+  static void Main ( string[] args )
+  {
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Precione qualquer tecla para fechar o programa");
+    Console.ReadLine();
+  }
+}
+
