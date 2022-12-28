5785807b4c7b3dad5e6d016516e60c6811369b92
Juliu Cesar <juliu2012@gmail.com>
2022-12-28 19:00:55 -0300

    new

---
+++ CSharp/Primeiros_passos/if_else/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  static void Main ( string[] args )
+  {
+
+    int idade = 17;
+    bool acompanhado = true;
+
+    if(idade >= 18){
+      
+    }
+
+
+    Console.WriteLine("Pressione qualquer tecla para sair...");
+    Console.ReadLine();
+  }
+}
