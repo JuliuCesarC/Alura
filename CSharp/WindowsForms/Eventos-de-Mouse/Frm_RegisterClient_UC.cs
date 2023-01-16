a8db198ad3d646a5b3ca8c6b9a66fc3e03990204
Juliu Cesar <juliu2012@gmail.com>
2023-01-16 09:38:44 -0300

    new

---
+++ CSharp/WindowsForms/Eventos-de-Mouse/Frm_RegisterClient_UC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios_Componente_e_Eventos
+{
+  public partial class Frm_RegisterClient_UC: UserControl
+  {
+    public Frm_RegisterClient_UC()
+    {
+      InitializeComponent();
+    }
+  }
+}
